8014757b720299a6ba591e9e2e0e13b5a0205ab3
pexiround <258477744+pexiround@users.noreply.github.com>
2026-04-12 14:39:47 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+export const config = {
+    name: "ZAPCZ",
+    bio: "Building the next generation of social identity. Explore my work below.",
+    avatar: "https://github.com/pexiround.png", // Pulls your GitHub photo automatically!
+    links: [
+        { label: "INSTAGRAM", url: "https://instagram.com" },
+        { label: "TWITTER / X", url: "https://twitter.com" },
+        { label: "GITHUB REPOS", url: "https://github.com/pexiround" },
+        { label: "GET THIS TEMPLATE", url: "https://github.com/pexiround/ZapczAI" }
+    ]
+};
